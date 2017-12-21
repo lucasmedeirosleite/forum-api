def5708bf54908a7be4f698e21ceb1a66cba18bf
Lucas Medeiros <lucastoc@gmail.com>
2017-12-21 12:28:32 -0300

    setup devise jwt

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source 'https://rubygems.org'
 
+ruby '2.4.2'
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
   "https://github.com/#{repo_name}.git"
@@ -19,6 +21,7 @@ gem 'rails', '~> 5.1.4'
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'dotenv-rails'
+  gem 'letter_opener_web'
   gem 'pry-byebug'
   gem 'rubocop'
 end
--- config/application.rb
+++ config/application.rb
@@ -3,24 +3,21 @@
 require_relative 'boot'
 
 require 'rails'
-# Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
-# require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'sprockets/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ForumApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    config.time_zone = 'UTC'
 
     config.autoload_paths += %W[#{config.root}/app/repositories
                                 #{config.root}/app/services
